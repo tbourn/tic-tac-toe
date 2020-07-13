8baf24603d99ed63f50e033d87db12d2d58bd5aa
thomas <thomas.bournaveas@gmail.com>
2020-07-13 19:10:34 +0300

    Added functionality

--- app/src/main/java/com/thomas/tictactoe/MainActivity.java
+++ app/src/main/java/com/thomas/tictactoe/MainActivity.java
@@ -5,8 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 0: yellow, 1: red, 2: empty
+
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPositions = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
+
+    private int activePlayer = 0;
+
+    private boolean gameActive = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +32,44 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationY(-1500);
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.setImageResource(R.drawable.yellow);
+        if (gameState[tappedCounter] == 2 && gameActive) {
+            gameState[tappedCounter] = activePlayer;
 
-        counter.animate().translationYBy(1500).setDuration(300);
+            counter.setTranslationY(-1500);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1500).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+
+                    // Someone has won!
+
+                    gameActive = false;
+
+                    String winner = "";
+
+                    if (activePlayer == 1) {
+                        winner = "Yellow";
+                    } else {
+                        winner = "Red";
+                    }
+
+                    Toast.makeText(this, winner + " has won!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
     }
 }
