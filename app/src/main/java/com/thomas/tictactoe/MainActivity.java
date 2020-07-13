38a4eec92feb875378a9f9f1cf8d30333ac8d5cc
thomas <thomas.bournaveas@gmail.com>
2020-07-13 18:42:31 +0300

    Added layout contents

--- app/src/main/java/com/thomas/tictactoe/MainActivity.java
+++ app/src/main/java/com/thomas/tictactoe/MainActivity.java
@@ -3,6 +3,8 @@ package com.thomas.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1500).setDuration(300);
     }
 }
